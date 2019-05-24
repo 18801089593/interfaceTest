45e1ec422a403c44863ad995696bf0097a365d1c
18801089593 <18801089593@163.com>
2019-05-24 22:51:34 +0800

    5.24

---
+++ report/yyy.py
@@ -0,0 +1,8 @@
+import os
+
+file = 'D:\interfaceTest\testDate\data.xls'
+
+# print(os.path.abspath(file))
+# print(os.path.basename(file))
+# print(os.path.dirname(file))
+print(os.path.split(file))
