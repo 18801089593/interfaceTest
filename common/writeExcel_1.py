80bc3e427bd8dfb04e07b6efee2a7db0cf4aaa8c
18801089593 <18801089593@163.com>
2019-05-31 23:58:22 +0800

    5.31

---
+++ common/writeExcel_1.py
@@ -0,0 +1,24 @@
+import xlrd
+from xlutils.copy import copy
+import os
+
+
+class writeExcel(object):
+
+    rb = xlrd.open_workbook(r'D:\interfaceTest\testDate\data.xls')
+    # rs = rb.sheet_by_index(2)
+    wb = copy(rb)
+    # 通过get_sheet()获取的sheet有write()方法
+    ws = wb.get_sheet(2)
+
+
+    def writeData(self,id,real,status):
+        try:
+            print('写入')
+            #根据id写入对应的实际结果和接口测试状态
+            self.ws.write(id,2,real)
+            self.ws.write(id,3,status)
+            self.wb.save('self.data_1.xls')
+            return 'ok'
+        except Exception as msg:
+            print(msg)
